b6ec810c34ec792bc880c9557f0d3da30a7ab63d
erlingcas <erlingc55@gmail.com>
2024-05-23 09:20:55 -0600

    clase practica de bases de datos

---
+++ SSISIntro.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE SSISIntro
+use SSISIntro
+
+create table Empleados (
+id int,
+Nombre Nvarchar(100),
+Apellidos Nvarchar (100),
+email Nvarchar (100),
+Departamento  Nvarchar(100)
+)
+
